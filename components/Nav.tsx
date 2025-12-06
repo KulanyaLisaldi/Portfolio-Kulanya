5098777d0414afaf4aad47a025fcb0ad1b573d69
Kulanya Lisaldi <kulanya.lisaldi@gmail.com>
2025-12-06 15:34:55 +0530

    update package.json

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -43,7 +43,7 @@ const Nav = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Logo */}
-          <div className="flex-shrink-0">
+          <div className="shrink-0">
             <a
               href="#home"
               onClick={(e) => handleNavClick(e, '#home')}
